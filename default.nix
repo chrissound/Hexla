bcb9019d50b93220dd67a46fd1e57ddfb9070014
Chris S <chris@soundshare.co.uk>
2020-05-24 12:45:23 +0100

    Pin nixpkgs - update to 20.03

--- default.nix
+++ default.nix
@@ -1,2 +1,13 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc864" } :
-nixpkgs.pkgs.haskell.packages.${compiler}.callCabal2nix "hexla" (./.) {}
+{ sources ? import ./nix/sources.nix
+} :
+let
+  niv = import sources.nixpkgs {
+    overlays = [
+      (_ : _ : { niv = import sources.niv {}; })
+    ] ;
+    config = {};
+  };
+  pkgs = niv.pkgs;
+  myHaskellPackages = pkgs.haskellPackages;
+in
+myHaskellPackages.callCabal2nix "hexla" (./.) {}
